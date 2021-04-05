8cbda0f97c91e8f128f698526633b1b6e1e8b58f
Olivera Micic <omicic@gmail.com>
2021-04-05 11:04:04 +0200

    header

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+    var typed = new Typed('.element', {
+        strings: ['Junior developer', '30 dana kodiranja', 'JS radionica', 'Custom WP radionica'],
+        loop: true,
+        typeSpeed: 100,
+        smartBackspace: true,
+        backSpeed: 100,
+        loopCount: Infinity
+    });
+
+//transition na ikonicu
+/* var image = document.getElementById("navbarimg");
+image.addEventListener('mouseover', function(){
+  image.src = "img/logo-plavi.png"
+})
+image.addEventListener('mouseout', function(){
+    image.src = "img/logo-beli.png"
+  }) */
